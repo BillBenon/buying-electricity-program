af7cbaabec3226037405652cbdc271bf08de3fe1
NickSingizwa <singizwanick19@gmail.com>
2021-06-15 15:48:48 +0200

    removing bugs

--- library/telecomtowers.c
+++ library/telecomtowers.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float telecom(float money,float tunits){
+struct boughtElectricityInfo{
+    float tunits;
+};
+
+struct boughtElectricityInfo telecom(float money){
         float tunits = money/201;
-    return tunits;
+        struct boughtElectricityInfo finalBuyingInfo;
+        finalBuyingInfo.tunits = tunits;
+        return finalBuyingInfo;
 }
--- library/watertreatment.c
+++ library/watertreatment.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float watertreatment(float money,float tunits){
+float watertreatment(float money){
         float tunits = money/126;
     return tunits;
 }
